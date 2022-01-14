8a31348af324667a906d26e8e3b2ee4b5ba380af
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-01-14 20:13:36 -0300

    Create content wrapper component

---
+++ src/components/_styled/ContentWrapper/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.main`
+  width: 90%;
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 30px 0;
+`;
+
+export default ContentWrapper;
