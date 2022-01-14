71da2b485ceeba7b0a8c9f2ce8b98e2f010e5d69
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-01-14 11:55:55 -0300

    Use ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import AppRoutes from './routes';
+import GlobalStyle from './styles/GlobalStyle';
+import appTheme from './styles/AppTheme';
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <ThemeProvider theme={appTheme}>
+      <GlobalStyle />
+      <AppRoutes />
+    </ThemeProvider>
+  );
 }
