553585c329436258b727feaa63b54ba77fa3aaa1
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-01-15 11:59:35 -0300

    Create list component

---
+++ src/components/List/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ListItem from '../ListItem';
+import ListWrapper from './styles';
+
+export default function List() {
+  return (
+    <ListWrapper>
+      {/* TEMPORARY!!! */}
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+      <ListItem />
+    </ListWrapper>
+  );
+}
---
+++ src/components/List/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const ListWrapper = styled.ul`
+  margin-top: 20px;
+  
+  @media (max-width: 600px) {
+    margin-top: 30px;
+  }
+`;
+
+export default ListWrapper;
