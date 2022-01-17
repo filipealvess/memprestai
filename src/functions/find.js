451ac0cc501388321847eb57d0dd0c204826e64f
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-01-17 20:51:33 -0300

    Create find id functions

--- src/functions/find.js
+++ src/functions/find.js
@@ -6,10 +6,26 @@ export function findClientName(id, clientList) {
   return foundClient.Nome;
 }
 
+export function findClientId(clientName, clientList) {
+  const foundClient = clientList.filter(client => client.Nome === clientName)[0];
+
+  if (foundClient === undefined) return null;
+
+  return foundClient.ID_Cliente;
+}
+
 export function findMovieTitle(id, movieList) {
   const foundMovie = movieList.filter(movie => movie.Id === id)[0];
   
   if (foundMovie === undefined) return null;
   
   return foundMovie.Titulo;
+}
+
+export function findMovieId(movieName, movieList) {
+  const foundMovie = movieList.filter(movie => movie.Titulo === movieName)[0];
+
+  if (foundMovie === undefined) return null;
+
+  return foundMovie.Id;
 }
